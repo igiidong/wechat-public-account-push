6808e7cb4a5bf82adabe2a816dabc0e7609d3202
igiidong <116710813+igiidong@users.noreply.github.com>
2022-10-26 17:01:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -40,7 +40,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2017-09-14'\},
+        { keyword: 'love_day', date: '2017-09-14'},
         
       ],
     },
