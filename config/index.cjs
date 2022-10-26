cf5a83ac573d884c36b48f7c4be68b667e6c9a63
igiidong <116710813+igiidong@users.noreply.github.com>
2022-10-26 16:59:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDPNj6q3jYxm05eU4lb7AT2UVld4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'KMkPJYlKyD_oIT9ftcd0HEL_zUWQDfpj_BS4c2TEggs',
+      useTemplateId: 'boJIVt7WMTeF1EI--JXruHCF9G9kPzxU4xnL6QAQJxU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '5-20',
       festivals: [
