49ae1377909c8ca95791b31ac5a1b80fb3d6aedf
igiidong <116710813+igiidong@users.noreply.github.com>
2022-10-26 17:05:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -50,13 +50,13 @@ const USER_CONFIG = {
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'boJIVt7WMTeF1EI--JXruHCF9G9kPzxU4xnL6QAQJxU',
 
-  //CALLBACK_USERS: [
-   // {
-     // name: '自己',
+  CALLBACK_USERS: [
+    {
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-     // id: 'oDPNj6r3Lks1yXYObbpdqhlh3OCg',
-    //}
-  //],
+      id: 'oDPNj6r3Lks1yXYObbpdqhlh3OCg',
+    }
+  ],
 
 }
 
